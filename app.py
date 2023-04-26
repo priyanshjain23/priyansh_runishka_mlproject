306b8d2c460d29a28532e39980c3fe3c2924fb76
priyanshjain23 <92178163+priyanshjain23@users.noreply.github.com>
2023-04-27 03:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ st.title('Will the user churn or not ')
 TotalCharges  = st.slider("TotalCharges ",0.00,2800.76)
 tenure = st.slider("tenure",0.000,100.01,step=0.0001,format="%.3f")
 MonthlyCharges = st.slider("MonthlyCharges",0.00,2500.0)
-Contract = st.slider("Contract",0,0,100.0)
-PaymentMethod = st.slider("PaymentMethod",0.0,10.0)
+Contract = st.slider("Contract",0,0,100.00)
+PaymentMethod = st.slider("PaymentMethod",0.0,10.00)
 # ProductRelated = st.slider("ProductRelated",0,200)
 
 #Pridiction function
